70953e9e77c27989ab84403cc886be7cf42bf0d9
Pavel Yusim <pyusim@PAVELs-MacBook-Pro.local>
2017-07-16 13:25:45 -0500

    Misc changes

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-var app=angular.module('myApp',[]);
+var app=angular.module('myApp',['services']);
 
-app.controller('mainController', ['$scope', function($scope) {
+app.controller('mainController', ['$scope', 'parserService', function($scope, parserService) {
 
 	    $scope.showText = "text to be shown";
 
+	    var input = "(id,created,employee(id,firstname,employeeType(id), lastname),location)";
+
+	    $scope.tokens = parserService.tokenizeLabels(input);
+
+	    $scope.parsed = parserService.parseLabels($scope.tokens);
 	}]);
